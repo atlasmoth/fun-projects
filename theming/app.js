9bfdd63bcbe7a9188bca4fc4d1ad2e6cf1d3b91d
atlasmoth <ebuka422@gmail.com>
2021-02-08 20:05:50 +0100

    working on twitter UI

--- theming/app.js
+++
@@ -1,5 +0,0 @@
-const component = document.querySelector("[data-theme]");
-
-component.addEventListener("click", (el) => {
-  component.setAttribute("data-theme", `color-${Math.ceil(Math.random() * 3)}`);
-});
